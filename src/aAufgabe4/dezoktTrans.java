5914e929c2a2028615ac7df7accdba584fcb2046
Ruben Krapf <krapfr@fh-trier.de>
2010-11-21 21:11:00 +0100

    New Aufgabe 2

---
+++ src/aAufgabe4/dezoktTrans.java
@@ -0,0 +1,90 @@
+package aAufgabe4;
+
+
+import java.util.Scanner;
+/*
+*     Aufgabenstellung Teil IV
+    Schreiben Sie ein Programm, welches eine Dezimalzahl (Basis 10) einliest und in eine Hexadezimal-zahl (Basis 16)
+    oder eine Oktalzahl (Basis 8) umwandelt und auf der Konsole ausgibt. Zunächst wird vom Benutzer eine Dezimalzahl verlangt.
+    Dann in welches Zahlenformat die eingegebene Zahl transformiert werden soll. Das Programm soll solange nach Dezimalzahlen fragen,
+    bis der Benutzer -1 eingibt. Die Transformation soll in zwei Methoden außerhalb der main-Methode erfolgen.
+*/
+ 
+public class dezoktTrans
+{
+    public static void main(String[] args)
+    {
+        //Deklaration Variablen
+        int dezimalzahl = 0, auswahl = 0;
+        
+        //Deklaration Objekte
+        Scanner myScanner = new Scanner(System.in);
+        
+        do
+        {
+            System.out.println("Bitte geben Sie eine Dezimalzahl ein: ");
+            dezimalzahl = myScanner.nextInt();                                     //Lesen der dezimalzahl mit Scanner
+            if (dezimalzahl !=-1)                                                //damit nicht der gesamte Text ausgeben wird wenn der Benutzer -1 zum Beenden eingibt
+            {
+            System.out.println("In welches Zahlenformat möchten Sie die Dezimalzahl "+dezimalzahl+" konvertieren? \n Bitte wählen Sie: \n 1 für Hexadezimal \n 2 für Octaln-1 zum beenden");
+            auswahl = myScanner.nextInt();
+                switch (auswahl)
+                {
+                    case 1:        System.out.println(convertToHex(dezimalzahl));    break;    
+                    case 2:        System.out.println(convertToOct(dezimalzahl));    break;
+                    default:    System.out.println("Fehler bei der Auswahl");     break;
+                }
+            }
+        }
+        while (dezimalzahl != -1);                                                //Abbruchbedingung
+        
+    }
+ 
+    //Methode convertToHex
+    //Integer.toHexString(dezimalzahl).toUpperCase()                            //Diese Methode macht das selbste von Haus aus
+    private static String convertToHex(int zahl)
+    {
+        
+        String hexaDezimalzahl = " ";
+        while (zahl != 0)
+        {
+            hexaDezimalzahl = intToLetterConverter(zahl%16) + hexaDezimalzahl;    //hexaDezimalzahl = Teilerrest(Dieses wird an die Methode zur Konvertierung übergeben) + "alte hexaDezimalzahl"    
+            zahl = zahl / 16;                                                    //Berechnung des Teilungserbnisses
+        }
+        return hexaDezimalzahl;
+ 
+ 
+    }
+    //Methode convertToOct
+    //Integer.toOctalString(dezimalzahl).toUpperCase()                            //Diese Methode macht das selbste von Haus aus
+    private static String convertToOct(int zahl)
+    {
+        String octalZahl = " ";
+        while (zahl != 0)                            
+        {
+            octalZahl = zahl%8 + octalZahl;                                        //octalZahl = Teilerrest + "alte OctalZahl"
+            zahl = zahl / 8;                                                    //Berechnung des Teilungserbnisses
+        }
+        return octalZahl;
+    }
+    
+    
+    //Konvertermethode damit die Zahlen 10 - 15 in Buchstaben konvertiert werden
+    private static String intToLetterConverter(int convert)
+    {
+        switch (convert)
+        {
+        case 10: return "A";                                                             //break nicht nötig, da hier nach dem return der Code sowieso nicht mehr erreichbar wäre    
+        case 11: return "B";                
+        case 12: return "C";                
+        case 13: return "D";                
+        case 14: return "E";                
+        case 15: return "F";                
+        default: return ""+convert;            
+        
+        }
+    }
+    
+}
+ 
+
